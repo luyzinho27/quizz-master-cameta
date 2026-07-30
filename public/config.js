da5623a40b1d0978af626b5f16152db83b4dd10c
luyzinho27 <luyz.carvalho.2024@gmail.com>
2026-07-29 21:46:12 -0300

    Delete public/config.js

--- public/config.js
+++
@@ -1,8 +0,0 @@
-window.QUIZZ_MASTER_CAMETA_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyCZcEfpP-TYxV3bVOQ7NsLTX_lufmBjUWY",
-  authDomain: "quizz-master-cameta.firebaseapp.com",
-  projectId: "quizz-master-cameta",
-  storageBucket: "quizz-master-cameta.firebasestorage.app",
-  messagingSenderId: "821906055328",
-  appId: "1:821906055328:web:98fd45a318fb5031b829c5"
-};
